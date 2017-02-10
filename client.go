8a65b8b9a0fd846ed3ebc404981ea9b42c3235f1
Alex Forbes-Reed <github@alx.red>
2017-02-10 01:54:23 +0000

    wrote function to parse data in a multiline response

--- client.go
+++ client.go
@@ -1,6 +1,10 @@
 package goxbdm
 
 import (
+	"strings"
+
+	"fmt"
+
 	"github.com/0xdeafcafe/go-xbdm/clients"
 )
 
@@ -36,4 +40,51 @@ func NewXBDMClientWithPort(xboxIP string, port int) (*Client, error) {
 	}
 
 	return client, nil
+}
+
+func parseMultilineResponse(str string) (map[string]string, error) {
+	body := make(map[string]string)
+
+	// Remove header and eom
+	splitStr := strings.Split(str, "\r\n")
+	dataLines := splitStr[1 : len(splitStr)-2]
+
+	// Join array with spaces, then add an extra space at the end to save last entry
+	dataStr := strings.Join(dataLines, " ") + " "
+	currentKey := ""
+	currentValue := ""
+	isInsideQuote := false
+	onKey := true
+	for _, char := range dataStr {
+		// If we detect a space, and we aren't inside a double quote, switch to `onKey`
+		if char == ' ' && !isInsideQuote {
+			// Before we switch we need to save the read values to the `body` map and
+			// reset the `currentKey` and `currentValue` variables
+			body[currentKey] = currentValue
+			currentKey = ""
+			currentValue = ""
+			onKey = true
+			continue
+		}
+
+		// If we find an equals, and we aren't inside a double quote, switch to `!onKey`
+		if char == '=' && !isInsideQuote {
+			onKey = false
+			continue
+		}
+
+		// If we find a double quote, switch between inside and outside
+		if char == '"' {
+			isInsideQuote = !isInsideQuote
+		}
+
+		// Save the value to the relevant part
+		if onKey {
+			currentKey = fmt.Sprintf("%s%s", currentKey, string(char))
+		} else {
+			currentValue = fmt.Sprintf("%s%s", currentValue, string(char))
+		}
+	}
+
+	return body, nil
 }
