f2277e6edc5f6c7d4467a43a685be220790aaeaa
Alex Forbes-Reed <github@alx.red>
2017-02-19 22:44:51 +0000

    wrote func for getting memory

--- commands_mem.go
+++ commands_mem.go
@@ -5,3 +5,13 @@ import "fmt"
 func (client *Client) SetMemory(address int64, data string) (string, error) {
 	return client.SendCommand(fmt.Sprintf(`setmem addr=0x%x data=%s`, address, data))
 }
+
+func (client *Client) GetMemory(address, length int64) ([]byte, error) {
+	_, err := client.SendCommand(fmt.Sprintf(`getmemex addr=0x%x length=%x`, address, length))
+	if err != nil {
+		return nil, err
+	}
+
+	data, _, err := client.tcpClient.Read(length)
+	return data, err
+}
